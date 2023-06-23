3b914cef4fe2803267ba7fb16f82ccf9736e7204
suchit1010 <sonisuchit144@gmail.com>
2023-06-23 14:27:21 +0530

    added frontend

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+import CreateTask from "./CreateTask";
+import Tasks from "./Tasks";
+function App() {
+  return <div></div>;
+}
+
+export default App;
