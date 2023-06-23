68502a56f01431c8d43d4182871fdcf1c824f0c3
suchit1010 <sonisuchit144@gmail.com>
2023-06-23 14:37:23 +0530

    added frontend

--- src/App.js
+++ src/App.js
@@ -3,8 +3,34 @@ import "./App.css";
 
 import CreateTask from "./CreateTask";
 import Tasks from "./Tasks";
-function App() {
-  return <div></div>;
-}
 
-export default App;
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (text, id) => ({
+    id,
+    text,
+    completed: false,
+  });
+
+  const addTask = (task) => {
+    const temp = [...tasks];
+    temp.push(createTask(task, tasks.length));
+    setTasks(temp);
+  };
+
+  const toggleCompleted = (id) => {
+    let temp = [...tasks];
+    const i = temp.findIndex((t) => t.id === id);
+    temp[i].completed = !temp[i].completed;
+    setTasks(temp);
+  };
+
+  return (
+    <div>
+    <CreateTask addTask={addTask} />
+    <Tasks tasks={tasks} toggleCompleted={toggleCompleted} />
+  </div>
+  )
+
+}
